2e07a869cef5c3dc3b603c5d6740333173a6c7a4
André Ribeiro <andre.ribeiro@ist.utl.pt>
2013-04-26 17:21:15 +0100

    XisEntityView queries and getReferenceEntities service added

--- xismobile.pim.uml2.gen.android/src/xismobile/pim/uml2/gen/android/services/Services.java
+++ xismobile.pim.uml2.gen.android/src/xismobile/pim/uml2/gen/android/services/Services.java
@@ -1,8 +1,21 @@
 package xismobile.pim.uml2.gen.android.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.uml2.uml.Association;
+import org.eclipse.uml2.uml.Class;
+
 
 public class Services {
 
-	// TODO: If needed...
-	
+	public List<String> getReferencedEntities(Class c) {
+		List<String> entities = new ArrayList<String>();
+
+		for (Association a : c.getAssociations()) {
+			entities.add(a.getEndTypes().get(1).getName());
+		}
+		return entities;
+	}
+
 }
