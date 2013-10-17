ac02cb2928fe74b9d383a092cf4597812a1c7374
André Ribeiro <andre.ribeiro@ist.utl.pt>
2013-10-17 18:05:10 +0100

    XisDropdownGestureManager added

---
+++ XIS-Mobile Library/src/xis_mobile/library/gestures/XisDropdownGestureManager.java
@@ -0,0 +1,25 @@
+package xis_mobile.library.gestures;
+
+public abstract class XisDropdownGestureManager implements XisGestureManager {
+	
+	public abstract void onTap();
+	
+	public abstract void onDoubleTap();
+	
+	public abstract void onLongTap();
+	
+	@Override
+	public void onPinch() {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void onStretch() {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void onSwipe() {
+		// TODO Auto-generated method stub
+	}
+}
