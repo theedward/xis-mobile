757208c736cf697b3c34aa19b473da4f2d304fdc
André Ribeiro <andre.ribeiro@ist.utl.pt>
2013-11-06 00:37:46 +0000

    Minor refactor

--- XISMobileEAPlugin/InteractionSpace/XisMenuItem.cs
+++ XISMobileEAPlugin/InteractionSpace/XisMenuItem.cs
@@ -25,6 +25,18 @@ namespace XISMobileEAPlugin.InteractionSpace
             }
         }
 
+        public string GetOnTapAction()
+        {
+            foreach (EA.TaggedValue tv in Element.TaggedValues)
+            {
+                if (tv.Name == "onTap")
+                {
+                    return tv.Value;
+                }
+            }
+            return null;
+        }
+
         public void SetOnTapAction(string onTap)
         {
             foreach (EA.TaggedValue tv in Element.TaggedValues)
@@ -36,18 +48,6 @@ namespace XISMobileEAPlugin.InteractionSpace
                     return;
                 }
             }
-        }
-
-        public string GetOnTapAction()
-        {
-            foreach (EA.TaggedValue tv in Element.TaggedValues)
-            {
-                if (tv.Name == "onTap")
-                {
-                    return tv.Value;
-                }
-            }
-            return null;
         }
     }
 }
