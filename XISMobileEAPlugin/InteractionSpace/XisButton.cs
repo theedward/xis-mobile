986096890b4399ff3010a7c817aaa10396fc4500
André Ribeiro <andre.ribeiro@ist.utl.pt>
2013-11-10 18:55:07 +0000

    XisButton update

--- XISMobileEAPlugin/InteractionSpace/XisButton.cs
+++ XISMobileEAPlugin/InteractionSpace/XisButton.cs
@@ -10,16 +10,22 @@ namespace XISMobileEAPlugin.InteractionSpace
         public XisButton(EA.Repository repository, XisWidget parent, EA.Diagram diagram,
             string name, string onTap = null) : base(repository)
         {
-            Element = XISMobileHelper.CreateXisButton(parent.Element, name, onTap);
-
             if (parent is XisInteractionSpace)
             {
                 XisInteractionSpace it = parent as XisInteractionSpace;
+                Element = XISMobileHelper.CreateXisButton(it.Element, name, onTap);
                 it.Widgets.Add(this);
+            }
+            else if (parent is XisVisibilityBoundary)
+            {
+                XisVisibilityBoundary boundary = parent as XisVisibilityBoundary;
+                Element = XISMobileHelper.CreateXisButton(boundary.Parent.Element, name, onTap);
+                boundary.Widgets.Add(this);
             }
             else if (parent is XisCompositeWidget)
             {
                 XisCompositeWidget comp = parent as XisCompositeWidget;
+                Element = XISMobileHelper.CreateXisButton(comp.Element, name, onTap);
                 comp.Widgets.Add(this);
             }
         }
