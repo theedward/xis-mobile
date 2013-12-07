8df21c58fbb909e9b780f735f9fe36bb4d46265b
André Ribeiro <andre.ribeiro@ist.utl.pt>
2013-12-07 22:10:26 +0000

    Context menu added

--- Case Studies/TourismApp/src/com/example/tourismapp/FavouritesActivity.java
+++ Case Studies/TourismApp/src/com/example/tourismapp/FavouritesActivity.java
@@ -3,13 +3,18 @@ package com.example.tourismapp;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 import com.example.tourismapp.domain.Favourite;
 import com.example.tourismapp.domain.OrmLiteHelper;
@@ -19,6 +24,7 @@ public class FavouritesActivity extends Activity {
 //	private DomainEntityManager manager;
 	private OrmLiteHelper helper;
 	private List<Favourite> favourites;
+	private ArrayAdapter<Favourite> favsAdapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +38,10 @@ public class FavouritesActivity extends Activity {
 		
 		if (favourites.size() > 0) {
 			ListView lv = new ListView(getApplicationContext());
-			lv.setAdapter(new ArrayAdapter<Favourite>(this,  android.R.layout.simple_list_item_1, favourites));
+			favsAdapter = new ArrayAdapter<Favourite>(this,  android.R.layout.simple_list_item_1, favourites);
+			lv.setAdapter(favsAdapter);
 			layout.addView(lv);
+			registerForContextMenu(lv);
 		} else {
 			TextView tv = new TextView(getApplicationContext());
 			tv.setText("No Favourites...");
@@ -56,5 +64,33 @@ public class FavouritesActivity extends Activity {
 		default:
 			return super.onOptionsItemSelected(item);
 		}
-	}	
+	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		getMenuInflater().inflate(R.menu.favourites_context, menu);
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		
+		switch (item.getItemId()) {
+		case R.id.view: 
+			Intent i = new Intent(getApplicationContext(), POIDetailActivity.class);
+			i.putExtra("POI", favourites.get(info.position).getName());
+			startActivity(i);
+			return true;
+		case R.id.delete:
+			Favourite f = favourites.get(info.position);
+			favourites.remove(info.position);
+			helper.deleteFavourite(f);
+			favsAdapter.notifyDataSetChanged();
+			return true;
+		default:
+			return super.onContextItemSelected(item);
+		}
+	}
 }
