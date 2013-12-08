3a3e0d45a7cd58e93c48c19d0dbed30bb319e61b
André Ribeiro <andre.ribeiro@ist.utl.pt>
2013-12-08 20:50:07 +0000

    NoteAdapter update/delete fix

--- Case Studies/To-DoListApp/src/xismobile/example/todolistapp/NoteAdapter.java
+++ Case Studies/To-DoListApp/src/xismobile/example/todolistapp/NoteAdapter.java
@@ -3,10 +3,12 @@ package xismobile.example.todolistapp;
 import java.util.List;
 
 import xismobile.example.todolistapp.domain.Note;
+import xismobile.example.todolistapp.domain.OrmLiteHelper;
 import xismobile.example.xistodoapp.R;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -16,7 +18,9 @@ import android.widget.EditText;
 public class NoteAdapter extends ArrayAdapter<Note> {
 
 	private Context mContext;
+	private OrmLiteHelper helper;
 	private int row;
+	private Note note;
 	private List<Note> notes;
 	
 	public NoteAdapter(Context context, int textViewResourceId, List<Note> list) {
@@ -24,6 +28,7 @@ public class NoteAdapter extends ArrayAdapter<Note> {
 		this.mContext = context;
 		this.row = textViewResourceId;
 		this.notes = list;
+		this.helper = OrmLiteHelper.getHelper(mContext);
 	}
 
 	@Override
@@ -57,11 +62,26 @@ public class NoteAdapter extends ArrayAdapter<Note> {
 		if ((notes == null) || ((position + 1) > notes.size()))
 			return convertView; // Can't extract item
 
-		Note obj = notes.get(position);
+		note = notes.get(position);
 		holder.description = (EditText) convertView.findViewById(R.id.editTextNoteDescription);
-		holder.description.setText(obj.getDescription());
+		holder.description.setText(note.getDescription());
 		holder.buttonUpdate = (Button) convertView.findViewById(R.id.buttonUpdate);
 		holder.buttonDelete = (Button) convertView.findViewById(R.id.buttonDelete);
+		holder.buttonUpdate.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				helper.createOrUpdateNote(note);
+				notifyDataSetChanged();
+			}
+		});
+		holder.buttonDelete.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				helper.deleteNote(note);
+				notes.remove(note);
+				notifyDataSetChanged();
+			}
+		});
 		
 		return convertView;
 	}
